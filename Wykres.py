b6aaa9b0f9b42ad6ca9de76c6cfb7919ee46adca
Andrzej <rajczyk.andrzej@gmail.com>
2025-05-31 12:26:36 +0200

    Wykres podpisy rozrzedzone

--- Wykres.py
+++ Wykres.py
@@ -19,7 +19,10 @@ axes[0].set_title("Rzeczywisty kurs Bitcoina")
 axes[0].set_xlabel("Data i czas")
 axes[0].set_ylabel("Cena (USD)")
 axes[0].legend()
-axes[0].tick_params(axis='x', rotation=45)
+step = 35
+xticks = df_actual["Data i czas"][::step]
+axes[0].set_xticks(xticks)
+axes[0].tick_params(axis='x', rotation=90)
 
 # Wykres estymowanego kursu
 axes[1].plot(df_estimated["Data i czas"], df_estimated["Przewidywana wartość"], label="Estymowany kurs", color="red")
@@ -27,7 +30,7 @@ axes[1].set_title("Estymowany kurs Bitcoina")
 axes[1].set_xlabel("Data i czas")
 axes[1].set_ylabel("Cena (USD)")
 axes[1].legend()
-axes[1].tick_params(axis='x', rotation=45)
+axes[1].tick_params(axis='x', rotation=90)
 
 # Wykres ostatnich 30 notowan i estymowanego kursu
 axes[2].plot(df_actual_last_30["Data i czas"], df_actual_last_30["Cena (USD)"], label="Ostatnie 30 notowań", color="blue")
@@ -36,7 +39,7 @@ axes[2].set_title("Ostatnie 30 notowań i estymowany kurs")
 axes[2].set_xlabel("Data i czas")
 axes[2].set_ylabel("Cena (USD)")
 axes[2].legend()
-axes[2].tick_params(axis='x', rotation=45)
+axes[2].tick_params(axis='x', rotation=90)
 
 plt.tight_layout()
-#plt.show()
+plt.show()
